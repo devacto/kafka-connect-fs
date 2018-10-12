318e73acad74e0b4453886f7b2cca1dcaf18eedb
Victor Wibisono <victor@eatigo.com>
2018-10-12 16:03:42 +0800

    Fixed bug on hasNext() function.

--- src/main/java/com/github/mmolimar/kafka/connect/fs/file/reader/FilenameFileReader.java
+++ src/main/java/com/github/mmolimar/kafka/connect/fs/file/reader/FilenameFileReader.java
@@ -6,16 +6,12 @@ import org.apache.hadoop.fs.Path;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class FilenameFileReader extends AbstractFileReader<FilenameFileReader.FilenameRecord> {
-    private static final Logger log = LoggerFactory.getLogger(FilenameFileReader.class);
-
     private final FilenameOffset offset;
     private String stringFilePath;
     private Schema schema;
@@ -23,7 +19,6 @@ public class FilenameFileReader extends AbstractFileReader<FilenameFileReader.Fi
     public FilenameFileReader(FileSystem fs, Path filePath, Map<String, Object> config) throws IOException {
         super(fs, filePath, new FilenameToStruct(), config);
         this.stringFilePath = filePath.toString();
-
         this.offset = new FilenameOffset(0);
         this.schema = SchemaBuilder.struct().field("filename", Schema.STRING_SCHEMA).build();
     }
@@ -43,11 +38,8 @@ public class FilenameFileReader extends AbstractFileReader<FilenameFileReader.Fi
     // Since we are not reading the file, we will always return false. No more line within the file.
     @Override
     public boolean hasNext() {
-        if (this.offset.getRecordOffset() == 0) {
+        if ((this.offset.getRecordOffset() == 0) || (this.offset.getRecordOffset() == 1)) {
             this.offset.setOffset(this.offset.getRecordOffset() + 1);
-            return true;
-        } else if (this.offset.getRecordOffset() == 1) {
-            // return true but not adding the record offset because then it will become 2 which throws an exception.
             return true;
         } else {
             return false;
@@ -58,7 +50,6 @@ public class FilenameFileReader extends AbstractFileReader<FilenameFileReader.Fi
     // Since we are not reading the file, this method is not being used.
     @Override
     public void seek(Offset offset) {
-        log.info("seek() to offset " + String.valueOf(offset.getRecordOffset()));
         if (offset.getRecordOffset() < 0) {
             throw new IllegalArgumentException("Record offset must be greater than 0");
         }
